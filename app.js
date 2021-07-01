a65566efb5d91ca048ba1d34ce0c8d8f39a8f9fc
Claudine McGowan <40922123+claudinemcg@users.noreply.github.com>
2021-07-01 13:59:19 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection; // set to db so its shorter to write
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', () => {
+    console.log('Database Connected');
+})
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    // res.send('Hello!')
+    res.render('home'); // home.ejs
+})
+
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds })
+})
+
+app.get('/campgrounds/:id', async (req, res) => { // show
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', { campground })
+})
+
+app.listen(3000, () => {
+    console.log('Serving on port 3000');
+})
