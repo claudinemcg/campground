7875bb390d96ab4460ff8e7f715376737febb148
Claudine McGowan <claudine.mcgowan@gmail.com>
2021-08-05 17:04:46 +0100

    Amended campground model and installed cookie-parser

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema // shorter to write
+const Review = require('./review')
 
 const CampgroundSchema = new Schema({
     title: String,
@@ -15,4 +16,21 @@ const CampgroundSchema = new Schema({
     ]
 })
 
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    // findOneAndDelete is a query middleware
+    // it only works because in app.js we have :
+    // app.delete..
+    //     await Campground.***findByIdAndDelete***(id); 
+    //     ...
+    // console.log(doc);
+    if (doc) { // this doc has reviews ...
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+                // ... and we are going to remove the reviews that match
+                //  the id(s) in the doc we just deleted
+            }
+        })
+    }
+})
 module.exports = mongoose.model('Campground', CampgroundSchema);
