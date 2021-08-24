f98676a44c8fcc724b4074f93a4f217dc3a63050
Claudine McGowan <claudine.mcgowan@gmail.com>
2021-08-24 13:46:53 +0100

    added option to add additional images in edit campground page

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -61,7 +61,10 @@ module.exports.renderEditForm = async (req, res) => { // show
 
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }) // spread req.body.campground into id object
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }); // spread req.body.campground into id object
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    campground.images.push(...imgs);
+    await campground.save();
     req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`)
 }
