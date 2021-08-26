5e21495ff4ce812f3e58435664591d47f093ff69
Claudine McGowan <claudine.mcgowan@gmail.com>
2021-08-26 12:15:29 +0100

    added campground name and location to popup and changed map to light version

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,11 +2,17 @@
 mapboxgl.accessToken = mapToken; // mapToken is a variable in show.ejd
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    style: 'mapbox://styles/mapbox/light-v10', // style URL
     center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 10 // starting zoom
 });
 
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
-    .addTo(map);
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h3>${campground.title}<h3> <p>${campground.location}<p>`
+            )
+    )
+    .addTo(map)
