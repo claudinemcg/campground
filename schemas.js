b1f4d82036477daea88c6d076315572d1ecba68c
Claudine McGowan <claudine.mcgowan@gmail.com>
2021-08-30 13:34:55 +0100

    installed sanitize-html and used it with Joi to prevent anyone from adding HTML to strings

--- schemas.js
+++ schemas.js
@@ -1,22 +1,49 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+const Joi = BaseJoi.extend(extension)
+
 // const campgroundSchema = Joi.object({
 module.exports.campgroundSchema = Joi.object({
     // this is not a mongoose schema- this is going to validate our data before it goes to mongoose
     campground: Joi.object({
         // campground is our key e.g. in form name= Campground[price]
-        title: Joi.string().required(),
+        title: Joi.string().required().escapeHTML(),
         price: Joi.number().required().min(0),
         // image: Joi.string().required(),
-        location: Joi.string().required(),
-        description: Joi.string().required()
+        location: Joi.string().required().escapeHTML(),
+        description: Joi.string().required().escapeHTML()
     }).required(),
     deleteImages: Joi.array()
 });
 
+
+
+
+
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().required().min(1).max(5),
-        body: Joi.string().required()
+        body: Joi.string().required().escapeHTML()
     }).required()
 });
 
