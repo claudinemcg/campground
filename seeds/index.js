0eaf80c82de2944eeb3a82e42df51631939bd310
Claudine McGowan <claudine.mcgowan@gmail.com>
2021-09-02 12:53:30 +0100

    change db to Mongo Atlas

--- seeds/index.js
+++ seeds/index.js
@@ -8,7 +8,8 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL;
+// const dbUrl = 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
